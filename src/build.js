0f84f9262490cefeb3e0fb859f5a4f9a731d1b69
Anton Shvydkoy <shvydkoy@techart.ru>
2016-12-29 17:33:31 +0300

    замена хеширования при билде

--- src/build.js
+++ src/build.js
@@ -5,28 +5,45 @@ const cliTools = new CliTools();
 
 class Build {
 	static needRebuild() {
-		return !(fse.existsSync(Build.sumFile) && !Build.sumChanged());
+		//return !(fse.existsSync(Build.sumFile) && !Build.sumChanged());
+		return !(fse.existsSync(Build.hashFile) && !Build.hashChanged());
 	}
 
 	static get sumFile() {
 		return `assets/sum.${Build.env}.md5`;
 	}
 
+	static get hashFile() {
+		return `assets/hash.${Build.env}.txt`;
+	}
 
 	static get md5sum() {
 		return cliTools.exec("tar cf - ./ --exclude='./node_modules*' --exclude='./assets*' --exclude='./img/sprite/sprite.png' --exclude='./src/style/_sprite.scss'|md5sum", false);
+	}
+
+	static get gitHash() {
+		return cliTools.exec("cd ../;git ls-tree -d HEAD frontend", false).toString().split(' ').pop().split('\t').shift();
 	}
 
 	static get sum() {
 		return fse.readFileSync(Build.sumFile, 'utf8')
 	}
 
+	static get hash() {
+		return fse.readFileSync(Build.hashFile, 'utf8')
+	}
+
 	static save() {
-		fse.writeFile(Build.sumFile, Build.md5sum);
+		//fse.writeFile(Build.sumFile, Build.md5sum);
+		fse.writeFile(Build.hashFile, Build.gitHash);
 	}
 
 	static sumChanged() {
 		return Build.sum != Build.md5sum.toString();
+	}
+
+	static hashChanged() {
+		return Build.hash != Build.gitHash;
 	}
 
 	static exists(env, dir) {
